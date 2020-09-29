9ab6e43d25752222694d887041feb904ccc4c7dc
Anton Johansson <anton.elis@hotmail.com>
2020-09-29 21:04:59 +0200

    Implemented Circumference and Area in Triangle class

--- ShapeLibrary/Triangle.cs
+++ ShapeLibrary/Triangle.cs
@@ -24,6 +24,18 @@ namespace ShapeLibrary
             this.SecondCorner = new Vector2(p2.X, p2.Y);
             this.ThirdCorner = new Vector2(p3.X, p3.Y);
 
+            // https://www.youtube.com/watch?list=PLshJtXCW6HtWsl-thJ6LWBRGTwP3GdWJx&time_continue=8&v=weW3SHQ9K8g&feature=emb_logo
+            float FirstSideWithFirstAndSecondCorner = (float)Math.Sqrt((float)Math.Pow((p2.X - p1.X), 2) + (float)Math.Pow((p2.Y - p1.Y),2));
+            float SecondSideWithSecondAndThirdCorner = (float)Math.Sqrt((float)Math.Pow((p3.X - p2.X), 2) + (float)Math.Pow((p3.Y - p2.Y), 2));
+            float ThirdSideWithThirdAndFirstCorner = (float)Math.Sqrt((float)Math.Pow((p1.X - p3.X), 2) + (float)Math.Pow((p1.Y - p3.Y), 2));
+
+            Circumference = FirstSideWithFirstAndSecondCorner + SecondSideWithSecondAndThirdCorner + ThirdSideWithThirdAndFirstCorner;
+            var SemiParameter = Circumference / 2;
+
+            this.Area = (float)Math.Sqrt(SemiParameter * (SemiParameter - FirstSideWithFirstAndSecondCorner) * (SemiParameter - SecondSideWithSecondAndThirdCorner) * (SemiParameter - ThirdSideWithThirdAndFirstCorner));
+
+            // Center
+
 
         }
     }
