11e4173772eb09403fa018a447d717ea51d90db4
Anton Johansson <anton.elis@hotmail.com>
2020-09-29 15:59:08 +0200

    First commit with ShapeLibrary structure

---
+++ ShapeApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ShapeLibrary;
+using System.Numerics;
+
+namespace ShapeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector2 v2 = new Vector2(5.0f, 2.4f);
+            Console.WriteLine(v2.X);
+        }
+    }
+}
---
+++ ShapeLibrary/Shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Numerics;
+
+namespace ShapeLibrary
+{
+    public abstract class Shape
+    {
+        public abstract Vector3 Center { get; }
+        public abstract float Area { get; }
+    }
+
+}
